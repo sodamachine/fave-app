665c86a9dded24ce6154a893c722df2c48a4604a
Stephanie <stephanie.ohn@gmail.com>
2021-04-05 23:38:29 -0400

    sets up edit+show faves actions via get+patch http verbs to edit products

--- app/controllers/faves_controller.rb
+++ app/controllers/faves_controller.rb
@@ -10,13 +10,30 @@ class FavesController < ApplicationController
     end
 
     post '/faves' do
-        fave = Fave.create(params[:item])
-        redirect 'faves/#{@fave.id}'
+        fave = Fave.create(params[:fave])
+        redirect 'faves/#{fave.id}'
     end
 
     get '/faves/:id' do
         @fave = Fave.find_by(id: params[:id])
+        if !@fave
+            redirect '/faves'
+        end
         erb :'faves/show'
     end
+
+    get '/faves/:id/edit' do
+        @fave = Fave.find_by(id: params[:id])
+        if !@fave
+          redirect '/faves'
+        end
+        erb :'faves/edit'
+    end
+    
+    patch '/faves/:id' do
+        @fave = Fave.find_by(id: params[:id])
+        @fave.update(params[:fave])
+        erb :'faves/show'
+      end
 
 end
