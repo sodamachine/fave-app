804c63ab2d7d926fae1443339a679b2e086bf46a
Stephanie <stephanie.ohn@gmail.com>
2021-04-05 22:22:55 -0400

    sets up signup+show user views via get+post http verbs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if session[:user_id]
+          redirect "/users/#{session[:user_id]}"
+        end
+        erb :'users/signup'
+    end
+
+    post '/users' do
+        u = User.create(params[:user])
+        session[:user_id] = u.id
+        redirect "/users/#{u.id}"
+    end
+
+    get '/users/:id' do
+        @user = User.find_by(id: params[:id])
+        erb :'users/show'
+    end
+
+
+
+
 end
