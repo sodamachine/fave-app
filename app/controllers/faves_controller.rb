21cba7099ceb579195ba0204fadce55413fa3585
Stephanie <stephanie.ohn@gmail.com>
2021-04-05 22:05:11 -0400

    sets up new action via get+post http verbs to add and view the newly added fave

--- app/controllers/faves_controller.rb
+++ app/controllers/faves_controller.rb
@@ -5,4 +5,13 @@ class FavesController < ApplicationController
         erb :'faves/index'
     end
 
+    get '/faves/new' do
+        erb :'faves/new'
+    end
+
+    post '/faves' do
+        fave = Fave.create(params[:item])
+        redirect 'faves/#{@fave.id}'
+    end
+
 end
